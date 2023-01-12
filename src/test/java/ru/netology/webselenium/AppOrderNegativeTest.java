fa5516036f410455330d95294dbeee218e988348
Liliya Yapparova <yapparovalili@yandex.ru>
2023-01-12 15:20:29 +0700

    ФÐAdd new test

--- src/test/java/ru/netology/webselenium/AppOrderNegativeTest.java
+++ src/test/java/ru/netology/webselenium/AppOrderNegativeTest.java
@@ -70,5 +70,42 @@ public class AppOrderNegativeTest {
         String actual = driver.findElement(By.cssSelector(".input_invalid[data-test-id=name] span.input__sub")).getText().trim();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldTestNegativePath4() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Сергей Иванов");
+        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+7913-848-35-83");
+        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector(".input_invalid[data-test-id=phone] span.input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestNegativePath5() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Анна Иванова");
+        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("Номер");
+        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector(".input_invalid[data-test-id=phone] span.input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestNegativePath6() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+78005553535");
+        //driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        String actual = driver.findElement(By.cssSelector("label[data-test-id=agreement].input_invalid")).getText().trim();
+        assertEquals(expected, actual);
+    }
 }
+
 
